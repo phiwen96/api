4c756b692537c7f9ec9858c975d1f7afb1d14d09
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2022-01-10 11:34:20 +0100

    a

--- modules/Http.cpp
+++ modules/Http.cpp
@@ -2,8 +2,33 @@ export module Http;
 
 export 
 {
-	struct http 
+	template <typename Http_request_type, typename Http_version, typename Url> 
+	struct http_request_line
 	{
-		
+		http_request_line (Http_request_type const& type, Http_version const& version, Url const& url) : _type {type}, _version {version}, _url {url}
+		{
+
+		}
+
+	private:
+		Http_request_type _type;
+		Http_version _version;
+		Url _url;
+	};
+
+
+
+	template <typename Http_version, typename Http_status_code, typename Http_status_phrase>
+	struct http_response_status_line
+	{
+		http_response_status_line (Http_version const& version, Http_status_code const& code, Http_status_phrase const& phrase) : _version {version}, _code {code}, _phrase {phrase}
+		{
+
+		}
+
+	private:
+		Http_version _version;
+		Http_status_code _code;
+		Http_status_phrase _phrase;
 	};
 }
